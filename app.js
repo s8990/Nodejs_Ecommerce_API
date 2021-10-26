c78eebbfb094518a40187a15b2e769b3ca76c59b
saeed <s8990@yahoo.com>
2021-10-26 21:01:03 +0330

    add errorHandler middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv/config');
 const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/errorHandler');
 app.use(cors());
 app.options('*', cors());
 
@@ -12,6 +13,7 @@ app.options('*', cors());
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(authJwt());
+app.use(errorHandler);
 
 // Routes
 const authRoutes = require('./routes/auth');
