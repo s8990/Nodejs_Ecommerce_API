052e7c09b50e9cd14a85226e30f2764cf6d2a0d0
saeed <s8990@yahoo.com>
2021-10-26 11:50:10 +0330

    add username to User model - add create User route

--- routes/users.js
+++ routes/users.js
@@ -13,4 +13,26 @@ router.get(`/`, async (req, res) => {
     res.status(200).json(users);
 });
 
+router.post(`/`, async (req, res) => {
+    let user = new UserModel({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        passwordHashed: req.body.passwordHashed,
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        address: req.body.address,
+        postalCode: req.body.postalCode,
+        city: req.body.city,
+        country: req.body.country,
+    });
+    user = await user.save();
+
+    if (!user) {
+        return res.status(400).send('Creating user failed!');
+    }
+
+    res.send(user);
+});
+
 module.exports = router;
