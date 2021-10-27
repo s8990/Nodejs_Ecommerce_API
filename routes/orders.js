61cd86ef9435a042fb4449a6500612c3714e1b27
saeed <s8990@yahoo.com>
2021-10-27 14:03:46 +0330

    Add totalPrice to Order

--- routes/orders.js
+++ routes/orders.js
@@ -46,6 +46,20 @@ router.post(`/`, async (req, res) => {
         );
         const orderItemsIdsResolved = await orderItemsIds;
 
+        const totalPrices = await Promise.all(
+            orderItemsIdsResolved.map(async (orderItemId) => {
+                const orderItem = await OrderItemModel.findById(
+                    orderItemId
+                ).populate('product', 'price');
+                const totalPrice = orderItem.product.price * orderItem.quantity;
+                return totalPrice;
+            })
+        );
+
+        const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
+
+        console.log(totalPrice);
+
         console.log(orderItemsIds);
         let order = new OrderModel({
             orderItems: orderItemsIdsResolved,
@@ -55,7 +69,7 @@ router.post(`/`, async (req, res) => {
             country: req.body.country,
             phone: req.body.phone,
             status: req.body.status,
-            totalPrice: req.body.totalPrice,
+            totalPrice: totalPrice,
             user: req.body.user,
         });
         order = await order.save();
