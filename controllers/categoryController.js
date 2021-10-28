f258dfcf4242122d33c9bc7ae40aecc1a25ab55a
saeed <s8990@yahoo.com>
2021-10-29 00:51:28 +0330

    Add Category controller

---
+++ controllers/categoryController.js
@@ -0,0 +1,84 @@
+const CategoryModel = require('../models/Category');
+
+const getCategories = async (req, res) => {
+    const categories = await CategoryModel.find();
+
+    if (!categories) {
+        res.status(500).json({ success: false });
+    }
+
+    res.status(200).json(categories);
+};
+
+const getCategoryById = async (req, res) => {
+    const category = await CategoryModel.findById(req.params.id);
+
+    if (!category) {
+        res.status(500).json({
+            success: false,
+            message: 'Category not found!',
+        });
+    }
+
+    res.status(200).json(category);
+};
+
+const addCategory = async (req, res) => {
+    let category = new CategoryModel({
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color,
+    });
+    category = await category.save();
+
+    if (!category) {
+        return res.status(400).send('Creating category failed!');
+    }
+
+    res.send(category);
+};
+
+const editCategory = async (req, res) => {
+    const category = await CategoryModel.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            icon: req.body.icon,
+            color: req.body.color,
+        },
+        { new: true }
+    );
+
+    if (!category) {
+        return res.status(500).send('Updating category failed!');
+    }
+
+    res.send(category);
+};
+
+const deleteCategory = (req, res) => {
+    CategoryModel.findByIdAndRemove(req.params.id)
+        .then((category) => {
+            if (category) {
+                return res.status(200).json({
+                    success: true,
+                    message: 'The category is deleted',
+                });
+            } else {
+                return res
+                    .status(404)
+                    .json({ success: false, message: 'Category not found!' });
+            }
+        })
+        .catch((error) => {
+            return res.status(400).json({ success: false, error: error });
+        });
+};
+
+module.exports = {
+    getCategories,
+    getCategoryById,
+    addCategory,
+    editCategory,
+    deleteCategory,
+};
--- routes/categories.js
+++ routes/categories.js
@@ -1,82 +1,16 @@
-const { response } = require('express');
 const express = require('express');
 const router = express.Router();
 
-const CategoryModel = require('../models/Category');
+const categoryController = require('../controllers/categoryController');
 
-router.get(`/`, async (req, res) => {
-    const categories = await CategoryModel.find();
+router.get(`/`, categoryController.getCategories);
 
-    if (!categories) {
-        res.status(500).json({ success: false });
-    }
+router.get(`/:id`, categoryController.getCategoryById);
 
-    res.status(200).json(categories);
-});
+router.post(`/`, categoryController.addCategory);
 
-router.get(`/:id`, async (req, res) => {
-    const category = await CategoryModel.findById(req.params.id);
+router.put('/:id', categoryController.editCategory);
 
-    if (!category) {
-        res.status(500).json({
-            success: false,
-            message: 'Category not found!',
-        });
-    }
-
-    res.status(200).json(category);
-});
-
-router.post(`/`, async (req, res) => {
-    let category = new CategoryModel({
-        name: req.body.name,
-        icon: req.body.icon,
-        color: req.body.color,
-    });
-    category = await category.save();
-
-    if (!category) {
-        return res.status(400).send('Creating category failed!');
-    }
-
-    res.send(category);
-});
-
-router.put('/:id', async (req, res) => {
-    const category = await CategoryModel.findByIdAndUpdate(
-        req.params.id,
-        {
-            name: req.body.name,
-            icon: req.body.icon,
-            color: req.body.color,
-        },
-        { new: true }
-    );
-
-    if (!category) {
-        return res.status(500).send('Updating category failed!');
-    }
-
-    res.send(category);
-});
-
-router.delete('/:id', (req, res) => {
-    CategoryModel.findByIdAndRemove(req.params.id)
-        .then((category) => {
-            if (category) {
-                return res.status(200).json({
-                    success: true,
-                    message: 'The category is deleted',
-                });
-            } else {
-                return res
-                    .status(404)
-                    .json({ success: false, message: 'Category not found!' });
-            }
-        })
-        .catch((error) => {
-            return res.status(400).json({ success: false, error: error });
-        });
-});
+router.delete('/:id', categoryController.deleteCategory);
 
 module.exports = router;
